4f0f8f9a9f08eb6a501fb255a7f0a8cbb194127e
avivbuen <aviv.buen@gmail.com>
2018-08-16 23:27:51 +0300

    API Port Fix

--- lib/server.ts
+++ lib/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import * as http from 'http';
 //import * as fs from 'fs';
-const PORT = 3000;
+const PORT = (process.env.PORT || 5000);
 
 /*const httpsOptions = {
     key: fs.readFileSync('./config/key.pem'),
